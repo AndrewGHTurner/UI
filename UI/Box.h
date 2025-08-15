0bdc7bd146aa353d62576f68559c9297bcfd0b6a
Andrew Turner <andrewgh100@gmail.com>
2025-08-15 10:17:11 +0100

    First doxygen comment

--- UI/Box.h
+++ UI/Box.h
@@ -5,6 +5,10 @@ using namespace sf;
 #define BOX_H
 #include "UI_DLL_Interface.h"
 //2d box on the screen
+/**
+@brief represents an area on the screen
+@note antiorigin could be removed an calculated when needed instead. This would be slower but would remove 8 bytes from every TreeNode
+*/
 class UI_API Box {
 protected:
 	Vector2f origin;//top right
